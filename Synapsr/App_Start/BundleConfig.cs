f1a56c600b01a3b8852a47e34407d846551027a9
aodpi <balan.valeriu@live.com>
2016-05-19 23:29:36 +0300

    Add project files.

---
+++ Synapsr/App_Start/BundleConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Synapsr
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            //Scripts
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/modernizr-{version}.js"));
+
+            //Styles
+            bundles.Add(new StyleBundle("~/bundles/styles/local").Include("~/Content/Site.css"));
+            bundles.Add(new StyleBundle("~/bundles/styles/bootstrap").Include("~/Content/bootstrap.css"));
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
---
+++ Synapsr/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Slack.Webhooks;
+namespace Synapsr.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static SlackClient cl = new SlackClient("https://hooks.slack.com/services/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");
+        // GET: Home
+        public ActionResult Index()
+        {
+            SlackMessage msg = new SlackMessage();
+            msg.Text = "Anunt: Maine la ora 8:00 in blocul 3 se va desfasura expozitia Creatia Studenteilor UTM.";
+            cl.PostToChannels(msg, new List<string> { "general", "random" });
+            return View();
+        }
+    }
+}
